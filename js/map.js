a6ddf622862574bc2462b57291fcb865febd4efe
Rekaszo <szeremleyohlinreka@gmail.com>
2018-04-09 22:44:34 +0100

    Spotify feature added
    Spotify feature added to HTML slide 3

--- js/map.js
+++ js/map.js
@@ -4,6 +4,11 @@
 // myTypeID = property specifies the map type to display. The following map types are supported: ROADMAP, SATELLITE, HYBRID, and TERRAIN.
 // creates a new map inside the <div> element with id="map", using the parameters that are passed (mapOptions).
 
+// initMap function that initializes and adds the map when the web page loads. Use a script tag to include your own JavaScript which contains the initMap function.
+// getElementById() function to find the map div on the web page.
+// the code below constructs a new Google maps object, and adds properties to the map including the center and zoom level. See the documentation for other property options.
+// new google.maps.Map() creates a new Google maps object
+// zoom property specifies the zoom level for the map. Zoom: 0 is the lowest zoom, and displays the entire earth. Set the zoom value higher to zoom in to the earth at higher resolutions.
 
 function initMap() {
   var uluru = {lat: 47.481561, lng: 19.064652};
